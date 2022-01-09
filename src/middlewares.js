eef8268116f10a92f2b6d4ea91a055cb9300a7aa
yungyungGwon <gywn7724@gmail.com>
2022-01-09 16:51:16 +0900

    heroku 503 fix

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,5 @@
 import multer from "multer";
 
-const isHeroku = process.env.NODE_ENV === "production";
-
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
@@ -33,12 +31,10 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
-  storage: isHeroku ? s3ImageUploader : undefined,
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: {
     fileSize: 20000000,
   },
-  storage: isHeroku ? s3VideoUploader : undefined,
 });
